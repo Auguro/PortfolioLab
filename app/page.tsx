f20a53908b43212a8fcdeab264deeac0a3204812
Auguro <augusto.d.farias@gmail.com>
2026-03-16 08:31:32 -0300

    Tentativa de deploy Vercel: 6

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import PaginaPrincipal from "./components/PaginaPrincipal";
 import { lerCSV } from "./lib/lerDados";
 import { lerCDI } from "./lib/lerDados";
 
-export const dynamic = 'force-dynamic';
-
 export default function Home() {
   const dados = lerCSV();
   const cdi = lerCDI();
